7eb9b54a2f5de6e6ec8d6bea3fc85eeba292a31f
yoh-yamamura <yyoh864@gmail.com>
2020-11-15 12:01:13 +0000

    ログインとログアウト。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      log_in @user
       flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
     else
       render :new
     end
