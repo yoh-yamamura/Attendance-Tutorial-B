918a347e7687018b9110c9eb063a5254d047824d
yoh-yamamura <yyoh864@gmail.com>
2020-12-14 17:16:14 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index,:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
-  
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+
   def index
     @users = User.paginate(page: params[:page])
-  end  
+  end
 
   def show
   end
@@ -37,17 +37,33 @@ class UsersController < ApplicationController
       render :edit      
     end
   end
-  
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
 
+  def edit_basic_info
+  end
+
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      #
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+  redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
 
     # beforeフィルター
@@ -66,8 +82,13 @@ class UsersController < ApplicationController
       end
     end
 
-   # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
